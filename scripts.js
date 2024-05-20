dca9fb91093a222f3746661a26864e502ffd6c7f
Adam Leveille <114101515+Quazsi@users.noreply.github.com>
2024-05-20 19:45:04 -0300

    Add promptUser function
    When the botton is pressed this function will
    prompt the user until the choose a number between
    10-100

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+// Prompt user for grid size function
+let userInput = 10;
+function promptUser(){
+    let message = "What size of grid would you like? (MIN 10) (Max 100)"
+    do {
+        userInput = prompt(message, 10)
+        console.log(userInput);
+        userInput = parseInt(userInput);
+    } while (userInput <= 10 || userInput >= 100 || isNaN(userInput));
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 
 const container = document.querySelector("#container");
-
 const square = document.createElement("div");
 container.appendChild(square);
 square.setAttribute("class", "square");
