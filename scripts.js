6a6df20415f4c79aee91e62f408f0e980c845c0a
Adam Leveille <114101515+Quazsi@users.noreply.github.com>
2024-05-20 18:57:54 -0300

    Add create square div for appending to container

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const container = document.querySelector("#container");
+
+const square = document.createElement("div");
+container.appendChild(square);
+square.setAttribute("class", "square");
+
+});
